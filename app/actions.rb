7501d0ce7608768e07da8ae41187218d610eaff4
Henry Yoo <henryhs.yoo@gmail.com>
2015-10-22 21:06:50 +0000

    display the first result of an image for a given search word

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,4 @@
-# Homepage (Root path)
-get '/' do
+get '/' do 
+  @face = HTTParty.get('https://www.googleapis.com/customsearch/v1?key=AIzaSyAcwfwtxSfmJpGHn17ZgWD9cPfmh57q51w&cx=008706594706571137855:earcvruiqoc&q=face&searchType=image')
   erb :index
 end
