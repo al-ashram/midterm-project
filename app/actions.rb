83d75474d53f3bb58e2cd6cd16964c74185e55c0
Holly-jolly <holly.haixin.l@gmail.com>
2015-10-26 00:35:29 +0000

    Delete play.erb, about.erb, and change the percent match in the admin page to make it round to 0

--- app/actions.rb
+++ app/actions.rb
@@ -1,42 +1,29 @@
 DEFAULT_IMAGE_URL = "https://s3.amazonaws.com/37assets/svn/765-default-avatar.png"
 imgurClient = Imgur.new('c7d3f45cb9049a2')
 
-  def grab_random_pic
+def grab_random_pic
   pictures = Picture.order("Random()").limit(2)
-   @picture_one = pictures[0] || Picture.new(url_link:DEFAULT_IMAGE_URL)
-   @picture_two = pictures[1] || Picture.new(url_link:DEFAULT_IMAGE_URL)
-   #binding.pry
-   # @picture_one = Picture.order("Random()").first
-   #  loop do
-   #   @picture_two = Picture.order("Random()").first
-   #   break if @picture_two.id != @picture_one.id
-  end
+  @picture_one = pictures[0] || Picture.new(url_link:DEFAULT_IMAGE_URL)
+  @picture_two = pictures[1] || Picture.new(url_link:DEFAULT_IMAGE_URL)
+end
 
 def get_all_urls
   @url_array = Array.new
   Picture.all.each do |image|
-    @url_array.push(image.url_link)
+  @url_array.push(image.url_link)
   end
 end 
 
 
 def grab_random_pic
-pictures = Picture.order("Random()").limit(2)
- @picture_one = pictures[0] || Picture.new(url_link:DEFAULT_IMAGE_URL)
- @picture_two = pictures[1] || Picture.new(url_link:DEFAULT_IMAGE_URL)
- #binding.pry
- # @picture_one = Picture.order("Random()").first
- #  loop do
- #   @picture_two = Picture.order("Random()").first
- #   break if @picture_two.id != @picture_one.id
- #  end
+  pictures = Picture.order("Random()").limit(2)
+  @picture_one = pictures[0] || Picture.new(url_link:DEFAULT_IMAGE_URL)
+  @picture_two = pictures[1] || Picture.new(url_link:DEFAULT_IMAGE_URL)
 end
 
 
 # Homepage (Root path)
 get '/' do
-  # @random_index = rand(30)
-  # @face = HTTParty.get("https://www.googleapis.com/customsearch/v1?key=AIzaSyAcwfwtxSfmJpGHn17ZgWD9cPfmh57q51w&cx=008706594706571137855:earcvruiqoc&safe=medium&q=face&searchType=image&start=#{@random_index}")
   grab_random_pic
   get_all_urls
   # erb :slider
@@ -55,7 +42,6 @@ post '/submit' do
   @score = params[:points]
   @picture_one_id = params[:picture_one_id]
   @picture_two_id = params[:picture_two_id]
-  # binding.pry
   # Find if the pair of picture_one and picture_two is already in the pairs table
   if @pair = Pair.find_by("(picture_one_id =? AND picture_two_id =?) OR (picture_one_id =? AND picture_two_id =?)", params[:picture_one_id], params[:picture_two_id], params[:picture_two_id],params[:picture_one_id])
     old_score = @pair.score.to_f
@@ -80,15 +66,9 @@ get '/top_picks' do
   erb :top_picks
 end
 
-get '/about' do
-  erb :about
-end
-
 get '/upload_page' do
   @picture = Picture.new
   @current_url_1 = DEFAULT_IMAGE_URL
-  # @current_url_2 = DEFAULT_IMAGE_URL
-
   erb :upload
 end
 
